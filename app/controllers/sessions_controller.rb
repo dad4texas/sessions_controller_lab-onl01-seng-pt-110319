e655560ed48027a50cb64d9b878b0747f5b325a0
Warren Harper II <warrenharper2@att.net>
2020-04-18 18:36:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
     def new
     end
-  
+
     def create
       if params[:name].nil? || params[:name].empty?
         redirect_to :login
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
         redirect_to '/'
       end
     end
-  
+
     def destroy
       if !session[:name].nil?
         session.delete :name
